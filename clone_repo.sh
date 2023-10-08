70f804265256459b1090183bc9c4c2efd6866193
scott <scottsaunders@mac.com>
2023-10-08 04:50:20 -0400

    Update clone_repo.sh
    Renamed custom admin bar

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,7 +2,7 @@
 repos=(
   "https://github.com/ipodscott/acf-core.git"
   "https://github.com/ipodscott/simple-fade-in.git"
-  "https://github.com/ipodscott/admin-bar.git"
+  "https://github.com/ipodscott/custom-front-admin-bar.git"
   "https://github.com/ipodscott/Gutenberg-Gradient-Fix.git"
   "https://github.com/ipodscott/styles.git"
 )
